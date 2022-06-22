c729c33ce4a56e866125f74e3dcdb1da5b690a62
Nicolas Debras <nicodebras17@gmail.com>
2022-06-22 14:51:16 +0200

    add user test

---
+++ src/user_file.py
@@ -0,0 +1,34 @@
+import datetime
+import re
+
+
+
+class user:
+
+
+
+    def __init__(self, nom, email, DateNaissance):
+        self.email = email
+        self.nom = nom
+        self.DateNaissance = DateNaissance
+        self.regex = '^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$'
+
+    def printUser(self):
+        print(self.DateNaissance.strftime('%Y-%m-%d %H:%M:%S'))
+
+    def check_email(self, email):  
+        if(re.search(self.regex,email)):  
+            return True
+        else:  
+            return False 
+    
+    def isValid(self):
+        test =  datetime.date.today()  - self.DateNaissance
+        print(datetime.date.today().strftime('%Y-%m-%d %H:%M:%S'))
+        if ( test > datetime.timedelta(days=4745) and len(self.nom) !=  0 and self.check_email(self.email) == True) :
+            return True
+        return False
+
+
+bob = user("nicolas", 'nico@gmail.com', datetime.date(2015, 6, 29))
+print(bob.isValid())
